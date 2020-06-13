e65acf74caa1537e3b9b84f448401456c5a61a7c
Yoshihiro Tanaka <contact@cordea.jp>
2020-06-13 17:10:10 +0900

    use kotlinx.serialization

--- backend/src/main/java/jp/cordea/independence/Server.kt
+++ backend/src/main/java/jp/cordea/independence/Server.kt
@@ -2,9 +2,14 @@ package jp.cordea.independence
 
 import io.ktor.application.Application
 import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
 import io.ktor.routing.Routing
+import io.ktor.serialization.json
 
 fun Application.main() {
+    install(ContentNegotiation) {
+        json()
+    }
     install(Routing) {
         api()
     }
