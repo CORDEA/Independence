a845b91f71dc1d574b5f80a308cf448c9dabf518
Yoshihiro Tanaka <contact@cordea.jp>
2020-06-14 12:43:52 +0900

    add new fragment

---
+++ android/src/main/java/jp/cordea/independence/MainFragment.kt
@@ -0,0 +1,7 @@
+package jp.cordea.independence
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainFragment : Fragment(R.layout.main_fragment)
