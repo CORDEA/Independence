840dca89a0d4b668f2bee6f08206e3c203985f9a
Yoshihiro Tanaka <contact@cordea.jp>
2020-06-21 18:48:23 +0900

    define color API

--- backend/src/jp/cordea/independence/Api.kt
+++ backend/src/jp/cordea/independence/Api.kt
@@ -1,6 +1,25 @@
 package jp.cordea.independence
 
+import io.ktor.application.call
+import io.ktor.response.respond
 import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.routing.route
 
 fun Routing.api() {
+    route("api") {
+        get("colors") {
+            call.respond(
+                listOf(
+                    Color("Pink", "#E91E63"),
+                    Color("Purple", "#9C27B0"),
+                    Color("Indigo", "#3F51B5"),
+                    Color("Cyan", "#00BCD4"),
+                    Color("Green", "#4CAF50"),
+                    Color("Lime", "#CDDC39"),
+                    Color("Orange", "#FF9800")
+                )
+            )
+        }
+    }
 }
